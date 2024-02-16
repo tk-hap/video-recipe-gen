c21472ab0682178352edc84c3ac4c23bbc836125
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2024-02-17 00:07:52 +1300

    use RecipeGenerator class to return html and pdf

--- flask/main.py
+++ flask/main.py
@@ -1,13 +1,16 @@
 import os
-from flask import Flask, request, render_template, make_response, url_for
+from flask import Flask, request, render_template, make_response, url_for, send_file
 from video import validate_url, get_video_id, transcribe_video, validate_video_content
 from recipe import create_recipe
 from config import REDIS_HOST, REDIS_PORT, REQUEST_LIMIT, REQUEST_TIMEOUT_SECS
+from models import RecipeGenerator
 import redis
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
-
 app = Flask(__name__)
+
+recipe = RecipeGenerator(details=None, video_id=None)
+
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 
 @app.route("/")
@@ -24,8 +27,8 @@ def max_requests():
 def submit_video():
     url = request.form["video"]
     if validate_url(url):
-        video_id = get_video_id(url)
-        if not validate_video_content(video_id):
+        recipe.video_id = get_video_id(url)
+        if not validate_video_content(recipe.video_id):
             return render_template("invalid-recipe.html")
         # Check if the user has exceeded the rate limit
         if not redis_client.exists(request.remote_addr):
@@ -36,11 +39,24 @@ def submit_video():
             response.headers["HX-Redirect"] = url_for("max_requests")
             return response
 
-        recipe = create_recipe(transcribe_video(video_id))
+        recipe.details = create_recipe(transcribe_video(recipe.video_id))
+        recipe.generate_html()
         redis_client.incr(request.remote_addr)
-        return render_template("recipe.html", recipe=recipe)
+        return recipe.html_recipe
     else:
         return ("", 204)
+
+
+@app.route("/recipe/<video_id>/pdf")
+def export_pdf(video_id: str) -> bytes:
+    if recipe.video_id == video_id:
+        recipe.generate_pdf()
+        print(recipe.pdf_recipe)
+        return send_file(
+            recipe.pdf_recipe,
+            as_attachment=True,
+            download_name=f"{recipe.details.title}.pdf",
+        )
 
 
 @app.route("/recipe/url", methods=["POST"])
