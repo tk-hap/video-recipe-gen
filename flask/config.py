d66f5d206a2fb37bc5ee5d57b0f6d95cc6990447
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2024-02-18 15:25:05 +1300

    start with structlog

--- flask/config.py
+++ flask/config.py
@@ -1,4 +1,6 @@
 import os
+import logging
+import structlog
 
 # Get env variables
 REDIS_HOST = os.environ.get("REDISHOST", "localhost")
@@ -7,3 +9,11 @@ REQUEST_LIMIT = int(os.environ.get("REQUEST_LIMIT", 3))
 REQUEST_TIMEOUT_SECS = int(os.environ.get("REQUEST_TIMEOUT_SECS", 60))
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
 MAX_VIDEO_LENGTH = os.environ.get("MAX_VIDEO_LENGTH", "PT45M")
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+
+# Configure structured logging
+structlog.configure(
+    wrapper_class=structlog.make_filtering_bound_logger(getattr(logging, LOG_LEVEL))
+)
+logger = structlog.get_logger()
+
