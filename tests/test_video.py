b18b9abf720878cf542c76f985e47bbab7198014
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2024-03-28 23:41:17 +1300

    fix tests and stop testing validate_url func

--- tests/test_video.py
+++ tests/test_video.py
@@ -5,9 +5,22 @@ sys.path.insert(0, './flask')
 import video
 
 
-def test_validate_url():
-    assert video.validate_url("https://www.youtube.com/watch?v=dQw4w9WgXcQ") == True
-    assert video.validate_url("not a url") == False
+def test_get_video_id():
+    valid_urls = {
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ": "dQw4w9WgXcQ",
+        "https://youtu.be/6tvBVCm9lmg?si=VuQYxsVc6tvA26KH": "6tvBVCm9lmg",
+        "m.youtube.com/watch?v=S_7SE_Uzk-I": "S_7SE_Uzk-I",
+        "youtube.com/watch?v=vYu4Zu5Ra_k": "vYu4Zu5Ra_k"
+    }
+    invalid_urls = [
+        "this is a url",
+        "https://www.reddit.com/"
+    ]
+
+    for url, id in valid_urls.items():
+        assert video.get_video_id(url) == id
+    for url in invalid_urls:
+        assert video.get_video_id(url) == False
 
 def test_validate_video_content():
     # Cooking vid id
