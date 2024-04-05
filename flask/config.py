5f922d1db49bbbbfb37a7668df1a518b1612adf0
TK Hapuku <tekaanuh@gmail.com>
2024-04-05 22:52:56 +1300

    fix redis env var naming

--- flask/config.py
+++ flask/config.py
@@ -3,8 +3,8 @@ import logging
 import structlog
 
 # Get env variables
-REDIS_HOST = os.environ.get("REDISHOST", "localhost")
-REDIS_PORT = int(os.environ.get("REDISPORT", 6379))
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
 REQUEST_LIMIT = int(os.environ.get("REQUEST_LIMIT", 3))
 REQUEST_TIMEOUT_SECS = int(os.environ.get("REQUEST_TIMEOUT_SECS", 60))
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
