69abaf061c20d6eeac9f15e2531f6e91fe1c504c
TK Hapuku <tekaanuh@gmail.com>
2024-04-08 18:45:55 +1200

    Helm chart initial

--- flask/config.py
+++ flask/config.py
@@ -10,7 +10,7 @@ REQUEST_TIMEOUT_SECS = int(os.environ.get("REQUEST_TIMEOUT_SECS", 60))
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
 MAX_VIDEO_LENGTH = os.environ.get("MAX_VIDEO_LENGTH", "PT45M")
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
-
+#OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 # Configure structured logging
 structlog.configure(
     wrapper_class=structlog.make_filtering_bound_logger(getattr(logging, LOG_LEVEL)),
